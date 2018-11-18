967c97d4fde536f640d2c403207c8820e5c94739
VladFiliucov <filiucov@mail.ru>
2018-11-18 19:16:21 +0000

    Add Interface and gameplay

--- lib/interface.rb
+++ lib/interface.rb
@@ -18,4 +18,65 @@ class TerminalInterface
   def successfully_generated_surface
     puts "You successfully generated surface of the planet. Now it is divided by sectors"
   end
+
+  def display_user_menu
+    puts "============="
+    puts "> 0 For exit "
+    puts "> 1 To add another robot"
+    puts "> 2 To show robots info"
+    puts "============="
+  end
+
+  def get_user_choise
+    gets.to_i
+  end
+
+  def land_robot_start_message
+    puts "================================="
+    puts "Now we are going to land a robot"
+    puts "================================="
+  end
+
+  def facing_direction_message
+    puts "================================="
+    puts "Please enter which direction is robot facing. Available options (N, E, S, W) "
+    puts "================================="
+  end
+
+  def facing_direction_prompt
+    gets.chomp.to_s
+  end
+
+  def sequnece_of_commands_prompt
+    gets.chomp.to_s
+  end
+
+  def robot_coordinate_message(axios)
+    puts "================================="
+    puts "Please enter robot position on #{axios.upcase} axios"
+    puts "================================="
+  end
+
+  def display_robot_info(robot)
+    if robot.status == 'LOST'
+      puts "================================="
+      puts "Robot Lost"
+      puts "================================="
+    else
+      puts "================================="
+      puts "Robot is facing #{robot.current_facing_direction}"
+      puts "================================="
+    end
+  end
+
+  def enter_sequnece_of_commands_message
+    puts "================================="
+    puts "Enter sequence of commands in one line. Available options:"
+    puts "L - for turning robot Left"
+    puts "R - for turning robot Right"
+    puts "F - for moving robot one step forward"
+    puts "================================="
+    puts "Press Return when you are done"
+    puts "================================="
+  end
 end
--- lib/mars_invasion.rb
+++ lib/mars_invasion.rb
@@ -14,13 +14,71 @@ class MarsInvasion
       @interface.successfully_generated_surface
     end
 
-    # invaders_move_request
+    invaders_move_request
   end
 
   private
 
   def invaders_move_request
     loop do
+      @interface.display_user_menu
+
+      user_menu
     end
+  end
+
+  def user_menu
+    choise = @interface.get_user_choise
+
+    case choise
+    when 0
+      exit
+    when 1
+      land_robot
+    when 2
+      robots_info
+    end
+  end
+
+  def land_robot
+    @interface.land_robot_start_message
+    @interface.facing_direction_message
+    facing_direction = @interface.facing_direction_prompt
+    @interface.robot_coordinate_message('X')
+    x_position = @interface.get_user_choise
+    @interface.robot_coordinate_message('Y')
+    y_position = @interface.get_user_choise
+
+    robot = Robot.new(facing_direction)
+
+    @robots << robot
+    @surface.land_robot(robot, x_position, y_position)
+
+    @interface.enter_sequnece_of_commands_message
+    commands = @interface.sequnece_of_commands_prompt
+    execute_commands(robot, commands)
+  end
+
+  def robots_info
+    @robots.each do |robot|
+      @interface.display_robot_info(robot)
+    end
+  end
+
+  def execute_commands(robot, commands)
+    commands_array = normalize_commands(commands)
+    commands_array.each do |command|
+      robot.send_signal(command)
+    end
+  end
+
+  def normalize_commands(commands)
+    array_of_chars = commands.chars.map(&:upcase)
+    result = []
+    array_of_chars.each do |e|
+      result << e if ['L', 'R', 'F'].include?(e)
+    end
+
+    result
   end
 end
