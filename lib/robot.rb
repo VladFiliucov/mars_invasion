ca0147a1e94cf3f1464d014ebc72b31397f813e9
VladFiliucov <filiucov@mail.ru>
2018-11-18 13:28:26 +0000

    Robot#turn to change current_facing_direction from N to W

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,11 +1,16 @@
 class Robot
-  attr_reader :status
+  attr_reader :status, :current_facing_direction
 
-  def initialize
+  def initialize(current_facing_direction)
     @status = 'ACTIVE'
+    @current_facing_direction = current_facing_direction
   end
 
   def lost!
     @status = 'LOST'
+  end
+
+  def turn(side)
+    @current_facing_direction = 'W'
   end
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -3,12 +3,24 @@ require_relative '../lib/robot';
 
 describe Robot do
   describe '#lost!' do
-    let(:robot) { Robot.new }
+    let(:robot) { Robot.new('N') }
 
     it 'changes robot status to LOST' do
       expect { robot.lost! }.to change{ robot.status }.from('ACTIVE').to('LOST')
     end
 
     it 'resets current_sector'
+  end
+
+  describe '#turn' do
+    describe 'changes robots facing direction' do
+      it 'from North to West' do
+        robot = Robot.new('N')
+
+        robot.turn('L')
+
+        expect(robot.current_facing_direction).to eq('W')
+      end
+    end
   end
 end
